d7b97d1517562dadbd2c3e4866284b3fcfd47ba9
wadeyen0818 <wade_yen@arplanet.com.tw>
2024-06-01 10:01:31 +0800

    商品功能 – 實作查詢商品列表（基礎實作）

--- src/main/java/com/wadeyen/springbootmall/dao/ProductDao.java
+++ src/main/java/com/wadeyen/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.wadeyen.springbootmall.dao;
 import com.wadeyen.springbootmall.dto.ProductRequest;
 import com.wadeyen.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
